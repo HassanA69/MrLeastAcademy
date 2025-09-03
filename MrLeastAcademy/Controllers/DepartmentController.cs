e706f5c7f8824639ef6fdbca20bd600c0fc709f1
Hassan Abdelnaby <hassan.abdelnaby69@gmail.com>
2025-09-03 23:46:50 +0300

    feat(department): add department creation page and update controller/views

--- MrLeastAcademy/Controllers/DepartmentController.cs
+++ MrLeastAcademy/Controllers/DepartmentController.cs
@@ -6,11 +6,19 @@ namespace MrLeastAcademy.Controllers
     public class DepartmentController : Controller
     {
         AppDbContext context = new AppDbContext();
+
+        
         public IActionResult Index()
         {
             List<Department> departments = context.Departments.ToList();
 
             return View("Index",departments);
         }
+
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
     }
+   
 }
