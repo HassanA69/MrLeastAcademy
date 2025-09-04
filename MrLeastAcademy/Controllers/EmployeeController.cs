11de76379690df48b1609e9c9dbcad03faa29a92
Hassan Abdelnaby <hassan.abdelnaby69@gmail.com>
2025-09-04 03:20:07 +0300

    feat(employee): add employee page and controller actions

--- MrLeastAcademy/Controllers/EmployeeController.cs
+++ MrLeastAcademy/Controllers/EmployeeController.cs
@@ -8,12 +8,51 @@ namespace MrLeastAcademy.Controllers
     public class EmployeeController : Controller
     {
         AppDbContext context = new AppDbContext();
+
+        public IActionResult Index()
+        {
+            return View("Index", context.Employees.ToList());
+        }
+
+        // Handle link
+        public IActionResult Edit(int Id)
+        {
+            Employee employee = context.Employees.FirstOrDefault(x => x.Id == Id);
+
+            if (employee == null)
+                return NotFound("No Employee with this Id");
+            return View("Edit", employee);
+        }
+        // Save edits
+        [HttpPost]
+        public IActionResult SaveEdit(Employee newEmp)
+        {
+            var depExists = context.Departments.Any(x => x.Id == newEmp.DepartmentId);
+
+            if (newEmp.Name ==null || newEmp.Salary<0 || newEmp.Address==null || newEmp.JobTitle == null || newEmp.ImageUrl == null || !depExists)
+            {
+                return View("edit", newEmp);
+            }
+            
+            var oldEmp = context.Employees.FirstOrDefault(x => x.Id == newEmp.Id);
+            oldEmp.Name = newEmp.Name;
+            oldEmp.Salary = newEmp.Salary;
+            oldEmp.Address = newEmp.Address;
+            oldEmp.JobTitle = newEmp.JobTitle;
+            oldEmp.ImageUrl = newEmp.ImageUrl;
+            oldEmp.DepartmentId = newEmp.DepartmentId;
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Details(int Id)
         {
-           var employee = context.Employees
-                .Include(x => x.Department)
-                .FirstOrDefault(x => x.Id == Id);
-           
+            var employee = context.Employees
+                 .Include(x => x.Department)
+                 .FirstOrDefault(x => x.Id == Id);
+
             EmployeeCustomDataViewModel employeeCustomData = new EmployeeCustomDataViewModel();
             List<string> branches = new List<string>() { "Cairo", "Alex", "Assiut" };
 
